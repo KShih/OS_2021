ee72ef76085cc76be8adddbdc3056bd25fa8ef93
KShih <kontonsine@gmail.com>
2021-10-02 17:07:59 -0400

    [add] add -c, -d, hashmap

--- hw1/uniq.c
+++ hw1/uniq.c
@@ -1,8 +1,84 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "stddef.h"
 
+#define SIZE 10000
 char buf[512];
+
+///////////////////// HashMap implementation ////////////////////////
+struct DataItem {
+   int data;   
+   char *key;
+};
+
+struct DataItem* hashArray[SIZE]; 
+struct DataItem* dummyItem;
+struct DataItem* item;
+
+int hashCode(char *str) {
+    int key = 0;
+    while (*str) {
+        key += (int)*str;
+        str ++;
+    }
+   return key % SIZE;
+}
+
+struct DataItem *search(char *key) {
+   int hashIndex = hashCode(key);
+
+   while(hashArray[hashIndex] != NULL) {
+	
+      if(hashArray[hashIndex]->key == key)
+         return hashArray[hashIndex]; 
+			
+      ++hashIndex;
+		
+      hashIndex %= SIZE;
+   }        
+	
+   return NULL;        
+}
+
+void insert(char *key,int data) {
+
+   struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
+   item->data = data;  
+   item->key = key;
+
+   int hashIndex = hashCode(key);
+
+   while(hashArray[hashIndex] != NULL && strcpy(hashArray[hashIndex]->key, "") != 0) {
+      ++hashIndex;
+		
+      hashIndex %= SIZE;
+   }
+	
+   hashArray[hashIndex] = item;
+}
+
+void update(char *key,int data) {
+
+   struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
+   item->data = data;  
+   item->key = key;
+
+   int hashIndex = hashCode(key);
+	
+   hashArray[hashIndex] = item;
+}
+
+void display() {
+   int i = 0;
+	
+   for(i = 0; i<SIZE; i++) {
+	
+      if(hashArray[i] != NULL)
+         printf(1, "(%s,%d)\n",hashArray[i]->key,hashArray[i]->data);
+   }
+}
+/////////////////////////////////////////////////
 
 void uniq(int fd, char *name) {
     int i, n;
@@ -39,26 +115,158 @@ void uniq(int fd, char *name) {
     free(prev);
     
     if (n < 0) {
-        printf(1, "wc: read error\n");
+        printf(1, "uniq: read error\n");
+        exit();
+    }
+}
+
+void uniqDup(int fd, char *name) {
+    int i, n;
+    char *prev;
+    char *cur;
+    int line = 0;
+    int start = 0;
+    int w = 0;
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        for (i = 0; i < n; i++) {
+            w += 1;
+            if (buf[i] == '\n') {
+                line += 1;
+                cur = (char *) malloc(w);
+                int index = 0;
+                int end = start + w;
+                while (start < end) {
+                    cur[index] = buf[start];
+                    index ++;
+                    start ++;
+                }
+                w = 0;
+                if((item = search(cur)) != NULL){
+                    if (item->data != 2) {
+                        printf(1, "%s", cur);
+                        update(cur, 2);
+                    }
+                } else {
+                    insert(cur, 1);
+                }
+                prev = (char *) malloc(sizeof(cur));
+                strcpy(prev, cur);
+                free(cur);
+            }
+            
+        }
+        start = 0;
+    }
+    free(prev);
+    
+    if (n < 0) {
+        printf(1, "uniq: read error\n");
+        exit();
+    }
+}
+
+void uniqCnt(int fd, char *name) {
+    int i, n;
+    char *prev;
+    char *cur;
+    int line = 0;
+    int start = 0;
+    int prevCnt = 0;
+    int w = 0;
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        for (i = 0; i < n; i++) {
+            w += 1;
+            if (buf[i] == '\n') {
+                line += 1;
+                cur = (char *) malloc(w);
+                int index = 0;
+                int end = start + w;
+                while (start < end) {
+                    cur[index] = buf[start];
+                    index ++;
+                    start ++;
+                }
+                w = 0;
+                if(line != 1 && strcmp(prev, cur) != 0){
+                    printf(2, "%d %s", prevCnt, prev);
+                    prevCnt = 1;
+                } else {
+                    prevCnt ++;
+                }
+                prev = (char *) malloc(sizeof(cur));
+                strcpy(prev, cur);
+                free(cur);
+            }
+            
+        }
+        start = 0;
+    }
+    printf(2, "%d %s", prevCnt, prev);
+    free(prev);
+    
+    if (n < 0) {
+        printf(1, "uniq: read error\n");
         exit();
     }
 }
 
 int main(int argc, char *argv[]) {
     int fd, i;
+    char c;
+
+    dummyItem = (struct DataItem*) malloc(sizeof(struct DataItem));
+    dummyItem->data = -1;  
+    dummyItem->key = "";
 
     if (argc <= 1) {
         uniq(0, "");
         exit();
     }
 
-    for (i = 1; i < argc; i++) {
-        if ((fd = open(argv[i], 0)) < 0) {
-        printf(1, "uniq: cannot open %s\n", argv[i]);
-        exit();
+    if ((++argv)[0] && argv[0][0] == '-') {
+        while(argv[0][0] == '-')
+        {
+            while((c = *++argv[0]))
+            {
+                switch(c) {
+                    case 'i':
+                        break;
+                    case 'c':
+                        for (i = 1; i < argc-1; i++) {
+                            if ((fd = open(argv[i], 0)) < 0) {
+                                printf(1, "uniq: cannot open %s\n", argv[i]);
+                                exit();
+                            }
+                            uniqCnt(fd, argv[i]);
+                            close(fd);
+                        }
+                        display();
+                        break;
+                    case 'd':
+                        for (i = 1; i < argc-1; i++) {
+                            if ((fd = open(argv[i], 0)) < 0) {
+                                printf(1, "uniq: cannot open %s\n", argv[i]);
+                                exit();
+                            }
+                            uniqDup(fd, argv[i]);
+                            close(fd);
+                        }
+                        break;
+                }
+                exit();
+            }
         }
-        uniq(fd, argv[i]);
-        close(fd);
     }
+    else {
+        for (i = 0; i < argc-1; i++) {
+                if ((fd = open(argv[i], 0)) < 0) {
+                    printf(1, "uniq: cannot open %s\n", argv[i]);
+                    exit();
+                }
+                uniq(fd, argv[i]);
+                close(fd);
+        }
+    }
+
     exit();
 }
