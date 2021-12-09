7c3502465d73061784e7aa40195c3b500967aa3d
KShih <kontonsine@gmail.com>
2021-12-09 11:41:46 -0500

    [modify] remove unused comment

--- final-project/src/run.c
+++ final-project/src/run.c
@@ -19,37 +19,17 @@ unsigned int xorbuf(unsigned int *buffer, int size) {
 }
 
 int readFromFile(char* filePath, int blockSize, int blockCount) {
-
-    if (blockSize % 4 != 0) {
-        printf("%s\n", "BlockSize should be times of 4.");
-        return -1;
-    }
-    
     unsigned int result = 0;
 
     int fd = open(filePath, O_RDONLY);
-    // int buffer[blockSize/4];
     unsigned char buffer[blockSize];
-
-    // int size = read(fd, buffer, sizeof(buffer));
-    // while (size > 0){   
-    //     for (int i = 0; i < size / 4; i++) {
-    //         result ^= buffer[i];
-    //     }
-    //     size = read(fd, buffer, sizeof(buffer));
-    // }
-    // printf("%08x\n", result);
 
     int size = read(fd, buffer, sizeof(buffer));
     blockCount -= 1;
     while (size > 0 && blockCount > 0){   
-        // for (int i = 0; i < size / 4; i++) {
-        //     result ^= buffer[i];
-        // }
         size = read(fd, buffer, sizeof(buffer));
         blockCount -= 1;
     }
-    // printf("%08x\n", result);
     close(fd);
     return 0;
 }
@@ -80,12 +60,6 @@ int main(int argc, char *argv[]) {
     long long blockSize, blockCount;
     char* path = argv[1];
     char* mode = argv[2];
-    // char* blockSize = argv[3];
-    // char* blockCount = argv[4];
-    // printf("%s\n", path);
-    // printf("%s\n", mode);
-    // printf("%s\n", blockSize);
-    // printf("%s\n", blockCount);
     if (sscanf(argv[3], "%lld", &blockSize) != 1 || sscanf(argv[4], "%lld", &blockCount) != 1){
         printf("%s", "bockSize and blockCount should be int");
         return -1;
@@ -103,8 +77,8 @@ int main(int argc, char *argv[]) {
     endTime = time(NULL);
     double fileSize = blockCount/1024*blockSize/1024;
     double timeSpend = endTime-startTime;
-    printf("fileSize: %f\n", fileSize);
-    printf("timeSpend: %f\n", timeSpend);
+    printf("fileSize: %f MB\n", fileSize);
+    printf("timeSpend: %f seconds\n", timeSpend);
     printf("speed: %f MB/s\n", fileSize / timeSpend);
 
     return 0;
