85dd0cf25feff87868e47c1edd639f4b781680e5
Zhang Haodong <ZhangHaodong1223@gmail.com>
2021-12-09 14:49:48 -0500

    add run2 && fast

---
+++ final-project/fast.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+
+unsigned int xorbuf(unsigned int *buffer, int size) {
+    unsigned int result = 0;
+    for (int i = 0; i < size; i++) {
+        result ^= buffer[i];
+    }
+    return result;
+}
+
+int readFile(char* filePath) {
+    int blockSize = 32768;
+    unsigned int result = 0;
+
+    int fd = open(filePath, O_RDONLY);
+    int buffer[blockSize / 4];
+
+    int size = read(fd, buffer, sizeof(buffer));
+    while (size > 0){   
+        for (int i = 0; i < size / 4; i++) {
+            result ^= buffer[i];
+        }
+        size = read(fd, buffer, sizeof(buffer));
+    }
+    printf("%08x\n", result);
+
+    
+    printf("%08x\n", result);
+    close(fd);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    char* path = argv[1];
+    time_t start, end;
+    start = time(NULL);
+    readFile(path);
+    end = time(NULL);
+    printf("Run %f seconds\n", difftime(end, start));
+    return 0;
+}
---
+++ final-project/run2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+
+int getBlockCountInTenSeconds(char* filePath, int blockSize) {
+    time_t start;
+    int fd = open(filePath, O_RDONLY);
+    unsigned char buffer[blockSize];
+    int size;
+    int blockCount = 0;
+    start = time(NULL);
+    while ((size = read(fd, buffer, sizeof(buffer))) > 0 && time(NULL) - start < 5){   
+        blockCount += 1;
+    }
+    close(fd);
+    return blockCount;
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        printf("%s", "invalid params\n");
+        return -1;
+    }
+    char* path = argv[1];
+    int blockSize;
+
+    if (sscanf(argv[2], "%d", &blockSize) != 1){
+        printf("%s", "bockSize and blockCount should be int");
+        return -1;
+    }
+    time_t start, end;
+    start = time(NULL);
+    int blockCount = getBlockCountInTenSeconds(path, blockSize);
+    end = time(NULL);
+    printf("Run %f seconds\n", difftime(end, start));
+    printf("Block Count: %d\n", blockCount);
+    return 0;
+}
